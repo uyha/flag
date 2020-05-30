d282a3da6220f04e5e6aeec76237049e423256b0
Uy Ha <hchanuy@gmail.com>
2020-05-30 15:36:30 +0200

    Add compile warning flags, ignore visual studio files

--- tests/main.cpp
+++ tests/main.cpp
@@ -8,72 +8,42 @@ using namespace river;
 
 constexpr bool cehck_compile_error = true;
 
-TEST_CASE("Operations on enum with std::uint8_t") {
-  enum class Easy : std::uint8_t { _0 = 0b000, _1 = 0b001, _2 = 0b010, _3 = 0b011, _4 = 0b100 };
+TEST_CASE("Operations on an enum flag") {
+  enum class Enum : std::uint8_t { _0 = 0b000, _1 = 0b001, _2 = 0b010, _3 = 0b011, _4 = 0b100 };
 
   SECTION("Flip operators") {
-    CHECK(std::is_same_v<std::uint8_t, decltype(~Easy::_0)>);
-    CHECK(~Easy::_0 == 0b1111'1111);
-    CHECK(~Easy::_1 == 0b1111'1110);
-    CHECK(~Easy::_2 == 0b1111'1101);
+    CHECK(std::is_same_v<std::uint8_t, decltype(~Enum::_0)>);
+    CHECK(~Enum::_0 == 0b1111'1111);
+    CHECK(~Enum::_1 == 0b1111'1110);
+    CHECK(~Enum::_2 == 0b1111'1101);
   }
 
   SECTION("Binary operators") {
-    CHECK(std::is_same_v<std::uint8_t, decltype(Easy::_0 | Easy::_1)>);
-    CHECK((Easy::_0 | Easy::_1) == 0b001);
-    CHECK((Easy::_0 & Easy::_1) == 0b000);
-    CHECK((Easy::_3 ^ Easy::_4) == 0b111);
+    CHECK(std::is_same_v<std::uint8_t, decltype(Enum::_0 | Enum::_1)>);
+    CHECK((Enum::_0 | Enum::_1) == 0b001);
+    CHECK((Enum::_0 & Enum::_1) == 0b000);
+    CHECK((Enum::_3 ^ Enum::_4) == 0b111);
   }
 
   SECTION("Chainning operations") {
-    CHECK(std::is_same_v<std::uint8_t, decltype(Easy::_0 & Easy::_1 & Easy::_2)>);
-    CHECK((Easy::_1 & Easy::_2 & Easy::_3) == 0b000);
-    CHECK((Easy::_1 | Easy::_2 | Easy::_4) == 0b111);
-    CHECK((Easy::_1 ^ Easy::_2 ^ Easy::_4) == 0b111);
+    CHECK(std::is_same_v<std::uint8_t, decltype(Enum::_0 & Enum::_1 & Enum::_2)>);
+    CHECK((Enum::_1 & Enum::_2 & Enum::_3) == 0b000);
+    CHECK((Enum::_1 | Enum::_2 | Enum::_4) == 0b111);
+    CHECK((Enum::_1 ^ Enum::_2 ^ Enum::_4) == 0b111);
   }
 
   SECTION("Getting bits") {
-    CHECK(std::is_same_v<std::uint8_t, decltype(bit<0>(Easy::_1))>);
-    CHECK(bit<0>(Easy::_1));
-    CHECK(bit<1>(Easy::_3));
+    CHECK(std::is_same_v<std::uint8_t, decltype(bit<0>(Enum::_1))>);
+    CHECK(bit<0>(Enum::_1));
+    CHECK(bit<1>(Enum::_3));
 
-    CHECK_FALSE(bit<1>(Easy::_1));
-    CHECK_FALSE(bit<2>(Easy::_3));
+    CHECK_FALSE(bit<1>(Enum::_1));
+    CHECK_FALSE(bit<2>(Enum::_3));
   }
 }
 
-TEST_CASE("Operations on enum with std::uint32_t") {
-  enum class Easy : std::uint32_t { _0 = 0b000, _1 = 0b001, _2 = 0b010, _3 = 0b011, _4 = 0b100 };
+TEST_CASE("Operations on enum and literals") {
+  enum class Enum : std::uint8_t { _0 = 0b000, _1 = 0b001, _2 = 0b010, _3 = 0b011, _4 = 0b100 };
 
-  SECTION("Flip operators") {
-    CHECK(std::is_same_v<std::uint32_t, decltype(~Easy::_0)>);
-    CHECK(~Easy::_0 == 0b1111'1111'1111'1111'1111'1111'1111'1111);
-    CHECK(~Easy::_1 == 0b1111'1111'1111'1111'1111'1111'1111'1110);
-    CHECK(~Easy::_2 == 0b1111'1111'1111'1111'1111'1111'1111'1101);
-  }
-
-  SECTION("Binary operators") {
-    CHECK(std::is_same_v<std::uint32_t, decltype(Easy::_0 | Easy::_1)>);
-    CHECK((Easy::_0 | Easy::_1) == 0b001);
-    CHECK((Easy::_0 & Easy::_1) == 0b000);
-    CHECK((Easy::_3 ^ Easy::_4) == 0b111);
-  }
-
-  SECTION("Chainning operations") {
-    CHECK(std::is_same_v<std::uint32_t, decltype(Easy::_0 & Easy::_1 & Easy::_2)>);
-    CHECK((Easy::_1 & Easy::_2 & Easy::_3) == 0b000);
-    CHECK((Easy::_1 | Easy::_2 | Easy::_4) == 0b111);
-    CHECK((Easy::_1 ^ Easy::_2 ^ Easy::_4) == 0b111);
-  }
-
-  SECTION("Getting bits") {
-    CHECK(std::is_same_v<std::uint32_t, decltype(bit<0>(Easy::_1))>);
-    CHECK(bit<0>(Easy::_1));
-    CHECK(bit<1>(Easy::_3));
-    CHECK((bit<0>(Easy::_3) && bit<1>(Easy::_3)));
-
-    CHECK_FALSE(bit<1>(Easy::_1));
-    CHECK_FALSE(bit<8>(Easy::_1));
-    CHECK_FALSE(bit<2>(Easy::_3));
-  }
+  CHECK((Enum::_1 & 0b1111'1111) == 0b1);
 }
