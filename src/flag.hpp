59db9fa757b1e1dcd7e21bb5615c0e83d7594a51
Uy Ha <hchanuy@gmail.com>
2020-05-31 12:16:21 +0200

    Update clang-format to break templates declarations

--- src/flag.hpp
+++ src/flag.hpp
@@ -6,45 +6,57 @@ namespace river {
 template <typename T>
 concept Flag = std::is_enum_v<T> &&std::is_unsigned_v<std::underlying_type_t<T>>;
 
-template <Flag T> constexpr auto underlying_value(T enum_value) {
+template <Flag T>
+constexpr auto underlying_value(T enum_value) {
   return static_cast<std::underlying_type_t<T>>(enum_value);
 }
 
-template <Flag T> constexpr auto operator~(T value) {
+template <Flag T>
+constexpr auto operator~(T value) {
   return static_cast<std::underlying_type_t<T>>(~underlying_value(value));
 }
 
-template <Flag T> constexpr auto operator|(T lhs, T rhs) {
+template <Flag T>
+constexpr auto operator|(T lhs, T rhs) {
   return static_cast<std::underlying_type_t<T>>(underlying_value(lhs) | underlying_value(rhs));
 }
-template <Flag T> constexpr auto operator|(std::underlying_type_t<T> lhs, T rhs) {
+template <Flag T>
+constexpr auto operator|(std::underlying_type_t<T> lhs, T rhs) {
   return static_cast<std::underlying_type_t<T>>(lhs | underlying_value(rhs));
 }
-template <Flag T> constexpr auto operator|(T lhs, std::underlying_type_t<T> rhs) {
+template <Flag T>
+constexpr auto operator|(T lhs, std::underlying_type_t<T> rhs) {
   return static_cast<std::underlying_type_t<T>>(underlying_value(lhs) | rhs);
 }
 
-template <Flag T> constexpr auto operator&(T lhs, T rhs) {
+template <Flag T>
+constexpr auto operator&(T lhs, T rhs) {
   return static_cast<std::underlying_type_t<T>>(underlying_value(lhs) & underlying_value(rhs));
 }
-template <Flag T> constexpr auto operator&(std::underlying_type_t<T> lhs, T rhs) {
+template <Flag T>
+constexpr auto operator&(std::underlying_type_t<T> lhs, T rhs) {
   return static_cast<std::underlying_type_t<T>>(lhs & underlying_value(rhs));
 }
-template <Flag T> constexpr auto operator&(T lhs, std::underlying_type_t<T> rhs) {
+template <Flag T>
+constexpr auto operator&(T lhs, std::underlying_type_t<T> rhs) {
   return static_cast<std::underlying_type_t<T>>(underlying_value(lhs) & rhs);
 }
 
-template <Flag T> constexpr auto operator^(T lhs, T rhs) {
+template <Flag T>
+constexpr auto operator^(T lhs, T rhs) {
   return static_cast<std::underlying_type_t<T>>(underlying_value(lhs) ^ underlying_value(rhs));
 }
-template <Flag T> constexpr auto operator^(std::underlying_type_t<T> lhs, T rhs) {
+template <Flag T>
+constexpr auto operator^(std::underlying_type_t<T> lhs, T rhs) {
   return static_cast<std::underlying_type_t<T>>(lhs ^ underlying_value(rhs));
 }
-template <Flag T> constexpr auto operator^(T lhs, std::underlying_type_t<T> rhs) {
+template <Flag T>
+constexpr auto operator^(T lhs, std::underlying_type_t<T> rhs) {
   return static_cast<std::underlying_type_t<T>>(underlying_value(lhs) ^ rhs);
 }
 
-template <int index, Flag T> constexpr auto bit(T value) {
+template <int index, Flag T>
+constexpr auto bit(T value) {
   static_assert(index >= 0, "Negative index is invalid");
   static_assert(index < sizeof(T) * 8, "Index exceeds the size of the value");
   return static_cast<std::underlying_type_t<T>>(underlying_value(value) & 1u << index);
